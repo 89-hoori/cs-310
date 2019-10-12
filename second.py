df77c31d7635f3a49c63958b92ac699b3b07661b
hooria qaisar <hooria393@gmail.com>
2019-10-12 13:43:10 +0500

    My first commit/

---
+++ second.py
@@ -0,0 +1,43 @@
+prompt="Enter your name:"
+name=input(prompt)
+Enter your name:Chunk
+print(name)
+print("Hello Chunk")
+
+Hours=35
+Rate=2.75
+Pay=Hours*Rate
+print(Pay)
+
+width=17
+height=12.0
+width//2
+type(width)
+
+width/2.0
+type(width)
+
+height/3
+type(height)
+
+1+2*5
+type(11)
+
+C=30
+F=(3.0*1.8)+32
+print(F)
+
+qoutient7//3
+print(qoutient)
+remainder=7%3
+print(remainder)
+
+inp=input()
+some silly staff
+print(inp)
+
+x=5
+x+1
+print(x)
+2**1+1
+6+4//2
